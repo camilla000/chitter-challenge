7c156927079038d11461f1a16e4d2261d4b04f88
Camilla <camilla.lima000@gmail.com>
2021-06-25 23:20:38 +0100

    Set up data base and tests

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require_relative './lib/peep'
+
+class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  get '/' do
+    'Welcome to Chitter. Please sign up'
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :index
+  end
+
+  get '/peeps/new' do
+    erb :newpeep
+  end
+
+  post '/peeps' do
+    newpeep = params['newpeep']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (text) VALUES('#{newpeep}')")
+
+    redirect '/peeps'
+  end
+
+  run! if app_file == $0
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,18 @@
 require 'simplecov'
 require 'simplecov-console'
+require_relative './setup_test_database'
+
+
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
+
+
 
 ENV['RACK_ENV'] = 'test'
 
@@ -20,8 +33,9 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+    # puts
+    # puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    # puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+
 end
